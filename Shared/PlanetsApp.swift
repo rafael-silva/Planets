9942c8c081050c69518bb0c59fbe8440a2d067b0
rafael silva <rafael_torrica@hotmail.com>
2021-09-14 11:08:40 -0300

    Initial Commit

---
+++ Shared/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Shared
+//
+//  Created by Rafael Silva on 14/09/21.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
